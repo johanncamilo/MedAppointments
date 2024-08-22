72624786813f1612c52f8137ea6b699bb719b3c6
Johann Camilo Belosareos Colmenares <milodevcool@gmail.com>
2024-08-22 16:32:49 -0500

    Constructor wii

--- readme.md
+++ readme.md
@@ -7,3 +7,9 @@
 **Clase:** Modelo de un objeto
 
 **MODULARIDAD:** Dividir un Sistema en Subsistemas
+
+**Método Constructor:**
+* Crea instancias de una clase
+* se invoca con el new()
+* se llama igual que su clase poseedora
+* Java proporciona un metodo constructor por defecto si no se crea
--- src/Doctor.java
+++ src/Doctor.java
@@ -4,6 +4,14 @@ public class Doctor  {
     String name;
     String speciality;
 
+    Doctor() {
+        System.out.println("nuevo doctor sin nombre");
+    }
+
+    Doctor(String name) {
+        System.out.printf("Hola doctor %s", name);
+        System.out.println();
+    }
 
     public void showName() {
         System.out.println(name);
