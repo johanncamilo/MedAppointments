30e5396fe7cec7093adf3d65e57ef9f635ab03ce
Johann mac <milodevcool@gmail.com>
2024-09-01 10:10:50 -0500

    Reto class patient

---
+++ src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    String blood;
+    double height;
+    double weight;
+
+    Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
